4988670fd0220cd2b6e3e958a17b93b1ccbef8c4
dmndc <dinomandic@hotmail.com>
2019-12-23 22:20:15 -0600

    implement stack

---
+++ stack-array.js
@@ -0,0 +1,43 @@
+class Stack {
+  constructor() {
+    this.count = 0;
+    this.items = {};
+  }
+
+  push(element) {
+    this.items[this.count] = element;
+    this.count++;
+  }
+
+  pop() {
+    if (this.isEmpty()) {
+      return undefined;
+    }
+
+    this.count--;
+    const result = this.items[this.count];
+    delete this.items[this.count];
+    return result;
+  }
+
+  peek() {
+    if (this.isEmpty()) {
+      return undefined;
+    }
+
+    return this.items[this.count - 1];
+  }
+
+  isEmpty() {
+    return this.count === 0;
+  }
+
+  size() {
+    return this.count;
+  }
+
+  clear() {
+    this.count = 0;
+    this.items = {};
+  }
+}
