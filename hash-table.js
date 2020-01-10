65aad63e0ef5f362e11374e306136f2a81bc9a3a
dmndc <dinomandic@hotmail.com>
2020-01-09 23:45:14 -0600

    edit hash table

--- hash-table.js
+++ hash-table.js
@@ -6,4 +6,20 @@ export default class HashTable {
     this.toStrFn = toStrFn;
     this.table = {};
   }
+
+  loseloseHashCode(key) {
+    if (typeof key === 'number') {
+      return key;
+    }
+    const tableKey = this.toStrFn(key);
+    let hash = 0;
+    for (let i = 0; i < tableKey.length; i++) {
+      hash += tableKey.charCodeAt(i);
+    }
+    return hash % 37;
+  }
+
+  hashCode(key) {
+    return this.loseloseHashCode(key);
+  }
 }
